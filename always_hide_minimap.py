f58fa753056b86946addda8eb71cd038462ad31d
Yang Shuai <yangshuai@gmail.com>
2018-08-10 04:55:32 +0800

    Initial commit

---
+++ always_hide_minimap.py
@@ -0,0 +1,7 @@
+import sublime_plugin
+
+class MinimapSetting(sublime_plugin.EventListener):
+
+    def on_activated(self, view):
+        view.window().set_minimap_visible(False)
+        print(r"view.window().set_minimap_visible(False)")
